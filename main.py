1edffc13d1077e08b0e4d0a1c19d9690b05abacc
Vladimir Volobuev <58554800+VolobuevVV@users.noreply.github.com>
2024-11-22 05:35:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+'''
+import subprocess
+import sys
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+try:
+    import ultralytics
+except ImportError:
+    install("ultralytics")
+try:
+    import roboflow
+except ImportError:
+    install("roboflow")
+
+
+
+
+from roboflow import Roboflow
+
+rf = Roboflow(api_key="isLsTzINU55umtJ2JAS9")
+project = rf.workspace("volobuevs-workspace").project("transportdetection")
+version = project.version(9)
+dataset = version.download("yolov11")
+
+
+from ultralytics.utils.benchmarks import benchmark
+benchmark(model="yolo_transport_close.pt", data="TransportDetection-9\data.yaml", imgsz=640)
+'''
+
+from ultralytics import YOLO
+model = YOLO('yolo_transport_far.pt')
+model.export(format="onnx")
+model.export(format="tflite")
+model.export(format="mnn")
+model.export(format="ncnn")
+model('TransportDetection-9/valid/images')
