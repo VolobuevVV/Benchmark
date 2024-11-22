b6b6f85178f39e8b912597c98149dfb696f34c09
Vladimir Volobuev <58554800+VolobuevVV@users.noreply.github.com>
2024-11-22 05:37:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''
+
 import subprocess
 import sys
 def install(package):
@@ -17,17 +17,16 @@ except ImportError:
 
 
 from roboflow import Roboflow
-
 rf = Roboflow(api_key="isLsTzINU55umtJ2JAS9")
 project = rf.workspace("volobuevs-workspace").project("transportdetection")
 version = project.version(9)
 dataset = version.download("yolov11")
 
-
+'''
 from ultralytics.utils.benchmarks import benchmark
 benchmark(model="yolo_transport_close.pt", data="TransportDetection-9\data.yaml", imgsz=640)
-'''
 
+import ultralytics
 from ultralytics import YOLO
 model = YOLO('yolo_transport_far.pt')
 model.export(format="onnx")
@@ -35,3 +34,4 @@ model.export(format="tflite")
 model.export(format="mnn")
 model.export(format="ncnn")
 model('TransportDetection-9/valid/images')
+'''
